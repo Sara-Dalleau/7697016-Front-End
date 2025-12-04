847733c776e5c17988eee08de9c87d6e43b00c9d
Dalleau Sara <sara.dalleau@gmail.com>
2025-12-04 17:19:02 +0100

    ajout boucle for

---
+++ js/pieces.js
@@ -0,0 +1,32 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+// Création des balises 
+for (let i = 0; i < pieces.length; i++) {
+
+const article = pieces[i];
+const pieceElement = document.createElement("article");
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponible ? "En stock" : "Rupture de stock";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+//Rattachement de nos balises au DOM
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(pieceElement);
+pieceElement.appendChild(imageElement);
+pieceElement.appendChild(nomElement);
+pieceElement.appendChild(prixElement);
+pieceElement.appendChild(categorieElement);
+pieceElement.appendChild(descriptionElement);
+pieceElement.appendChild(disponibiliteElement);
+}
+
